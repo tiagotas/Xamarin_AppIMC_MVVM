6b0b352acd48ee84e85799ba45e10c9491b7225c
TIAGO SILVA <frost.tiago@gmail.com>
2020-11-18 18:50:36 -0300

    Adicionar arquivos de projeto.

---
+++ AppIMCMVVM/AppIMCMVVM/Model/Imc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppIMCMVVM.Model
+{
+    public class Imc
+    {
+
+        public static string CalcularImc(double peso, double altura)
+        {
+            double imc = peso / (altura * altura);
+
+            string classificacao = "";
+
+            if (imc < 17)
+            {
+                classificacao = "Muito abaixo do peso";
+
+            }
+            else if (imc >= 17 && imc < 18.49)
+            {
+                classificacao = "Abaixo do peso";
+
+            }
+            else if (imc >= 18.5 && imc < 24.99)
+            {
+                classificacao = "Peso normal";
+
+            }
+            else if (imc >= 25 && imc < 29.99)
+            {
+                classificacao = "Acima do peso";
+
+            }
+            else if (imc >= 30 && imc < 34.99)
+            {
+                classificacao = "Obesidade I";
+
+            }
+            else if (imc >= 35 && imc < 39.99)
+            {
+                classificacao = "Obesidade II (severa)";
+            }
+            else if (imc >= 40)
+            {
+                classificacao = "Obesidade III (mórbida)";
+            }
+
+            return "Seu IMC é " + imc.ToString(("0.00")) + " está " + classificacao;
+        }
+
+
+
+    }
+}
---
+++ AppIMCMVVM/AppIMCMVVM/ViewModel/FormularioIMCViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppIMCMVVM.ViewModel
+{
+    public class FormularioIMCViewModel : INotifyPropertyChanged
+    {
+        private double peso, altura;
+        private string resultado;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        public string Resultado
+        {
+            get { return resultado; }
+            set
+            {
+                resultado = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Resultado"));
+            }                
+        }
+
+        public ICommand CalcularIMCCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {       
+                    Resultado = Model.Imc.CalcularImc(peso, altura);
+                    //Console.WriteLine("Resultado = " + Resultado);
+                });
+            }
+        }        
+    }
+}
